e201bd596723a782a863c0c8230064884259a3dc
Evangelos Constantinou <vangelas87@googlemail.com>
2023-11-25 17:33:13 -0500

    sync t490 nov 25 2023

--- Tutorials/Discrete_Choice/pyblp_tutorial/example_1.py
+++ Tutorials/Discrete_Choice/pyblp_tutorial/example_1.py
@@ -1,30 +1,58 @@
 import pyblp
 import pandas as pd
 import re
+import numpy as np 
+import statsmodels.formula.api as smf
 
 # Load data
 product_data = pd.read_csv(pyblp.data.NEVO_PRODUCTS_LOCATION)
 product_data.head()
 product_data.columns
 
-product_data[
-        [i for i in product_data.columns if re.search('^((?!demand).)*$', i)]
-        ].head()
-
 
 product_data.loc[product_data.market_ids=='C01Q1',
         [i for i in product_data.columns if re.search('^((?!demand).)*$', i)]
-        ].head()
+        ]
+# --> market_ids account for city and quarter
+product_data.loc[product_data.market_ids=='C01Q1', ['city_ids', 'quarter', 'shares']]
+product_data.loc[product_data.market_ids=='C01Q1', 'shares'].sum()
 
 product_data.shape
 
 # Set up the problem
 logit_formulation = pyblp.Formulation('prices', absorb='C(product_ids)')
-logit_formulation
+logit_formulation = pyblp.Formulation('prices + sugar + mushy')
+logit_formulation = pyblp.Formulation('prices')
+print(logit_formulation)
 
 problem = pyblp.Problem(logit_formulation, product_data)
-problem
+print(problem)
 
 # Solving the problem
 logit_results = problem.solve()
-logit_results
+print(logit_results)
+
+
+
+print(1.085386E+01)
+print(+8.359402E-01)
+print(-3.004710E+01)
+print(+1.008589E+00)
+
+
+# =============
+# OLS
+product_data.columns
+small_df = product_data[
+        ['market_ids', 'city_ids', 'quarter',
+         'shares', 'prices', 'sugar', 'mushy']].copy()
+
+# Derive "outside option's share"; recall that market_ids includes city and qtr
+small_df.groupby(['market_ids'])['shares'].sum()
+small_df['s0'] = 1 - small_df.groupby(['market_ids'])['shares'].transform('sum')
+
+
+# Regression
+reg = smf.ols('np.log(shares/s0) ~ prices + sugar + mushy', data = small_df)
+res = reg.fit()
+print(res.summary())
